5c72644d786a59a16c0d5f7b8a330b9f4a0695f0
sarahcullinan <scullin2@nd.edu>
2024-04-08 20:30:32 -0400

    Vigenere decrypt

--- vigenere.py
+++ vigenere.py
@@ -29,8 +29,38 @@ def encrypt(plain_text, key):
 
     return encrypted_text
 
-# Example usage:
+def decrypt(cipher_text, key):
+     # Convert both the encrypted text and key to uppercase
+    cipher_text = cipher_text.upper()
+    key = key.upper()
+
+    plain_text = ""
+    key_index = 0
+
+    for ch in cipher_text:
+        if ch.isalpha():
+            A = 65 
+            # Shift the character back by the corresponding key character
+            shift = ord(key[key_index]) - A
+            plain_text += chr(((ord(ch) - A - shift) % 26) + A)
+
+            # Move to the next character in the key
+            key_index = (key_index + 1) % len(key)
+        else:
+            # If it's not a letter , just append it as it is
+            plain_text += ch
+
+    return plain_text
+
 plain_text = "Hello World"
 key = "KEY"
-encrypted_text = encrypt(plain_text, key)
-print("Encrypted text:", encrypted_text)
+cipher_text = encrypt(plain_text, key)
+print("Encrypted text:", cipher_text)
+
+cipher_text = "RIJVS UYVJN"
+key = "KEY"
+plain_text = decrypt(cipher_text, key)
+print("Decrypted text:", plain_text)
+
+
+
