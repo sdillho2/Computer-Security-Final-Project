7608dacd2afa5641c3264b9d811406cf5488c6c8
Katie Grace <42680309+katiegrace@users.noreply.github.com>
2024-04-10 12:51:53 -0400

    Update rsa_binaries.py

--- rsa_binaries.py
+++ rsa_binaries.py
@@ -24,18 +24,23 @@ d = int(input("Enter your private key (d): "))
 # Message to be encrypted
 # ADD IN USER INPUT TO GET THE MESSAGE TO ENCRYPT
 #msg = 12.0
-msg = int(input("Enter your message you want to encrypt: "))
-
+binary_msg = input("Enter your binary message you want to encrypt: ")
+msg = int(binary_msg, 2)  # Convert binary to integer
 print("Message data = ", msg)
 
 # for each 2 nubmers in msg, find the letter
 
 # Encryption c = (msg ^ e) % n
-c = pow(msg, e)
-c = math.fmod(c, n)
+# c = pow(msg, e)
+# c = math.fmod(c, n)
+# print("Encrypted data = ", c)
+c = pow(msg, e, n)  # Use built-in pow() with modulo
 print("Encrypted data = ", c)
 
+
 # Decryption m = (c ^ d) % n
-m = pow(c, d)
-m = math.fmod(m, n)
-print("Original Message Sent = ", m)
+# m = pow(c, d)
+# m = math.fmod(m, n)
+# print("Original Message Sent = ", m)
+m = pow(c, d, n)  # Use built-in pow() with modulo
+print("Original Message Sent (decrypted)= ", m)
