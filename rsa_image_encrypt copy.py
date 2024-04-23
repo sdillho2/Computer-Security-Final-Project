c8c11acd3bed4a673ed57f1246a469e95f7bb85f
Katie Grace <42680309+katiegrace@users.noreply.github.com>
2024-04-22 22:50:15 -0400

    rsa encrypt to ask for input

--- rsa_image_encrypt copy.py
+++ rsa_image_encrypt copy.py
@@ -1,31 +1,45 @@
-import random
-
-
 if __name__=="__main__":
     from PIL import Image
     import numpy
-    imgname=input('Enter image name: ')
+    
+    imgname = input('Enter image name: ')
     try:
-        img=Image.open(imgname).convert('L') 
-        imgarray=numpy.array(img)
-        fname=input('Enter destination encrypted file name: ')
-        with open(fname,'w') as fobj:
-            e=1
-            r,c=imgarray.shape
-            imgarray=imgarray.tolist()
-            n = int(input('Enter n: '))
-            d = int(input('Enter private key (d): '))
-            e = int(input('Enter public key (e): '))
-            fobj.write(str(r)+'^')
-            fobj.write(str(c)+'^')
+        img = Image.open(imgname).convert('L') 
+        imgarray = numpy.array(img)
+        fname = input('Enter destination encrypted file name: ')
+        with open(fname, 'w+') as fobj:
+            e = int(input("Enter the value of e: "))  # Ask user for e
+            r, c = imgarray.shape
+            imgarray = imgarray.tolist()
+            
+            p = int(input("Enter the value of p: "))  # Ask user for p
+            q = int(input("Enter the value of q: "))  # Ask user for q
+            
+            n = p * q
+            phi = (p - 1) * (q - 1)
+            
+            # No need to calculate d here, as it's calculated based on e and phi below
+            
+            print("Public key : ", e)
+            
+            d = 1
+            while True:
+                if ((d * e) - 1) % phi == 0 and d * e > 1:
+                    break
+                d += 1
+            
+            print("Private key = ", d)
+            print("n = ", n)
+            
+            fobj.write(str(r) + '^')
+            fobj.write(str(c) + '^')
+            
             for i in range(r):
                 for j in range(c):
-                    enc_value=(imgarray[i][j]**e)%n
-                    fobj.write(str(enc_value)+'^')
-            #print(f'Image encrypted as {fname}!!')
-            print('Image encrypted as ',fname,'!')
+                    enc_value = (imgarray[i][j] ** e) % n
+                    fobj.write(str(enc_value) + '^')
+            
+            print('Image encrypted as ', fname, '!')
+    
     except IOError:
         print('Image not found!')
-
-
-
